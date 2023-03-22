700f1453cd4b88d68db256b311424186d27db357
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-03-23 01:36:23 +0300

    mvp tamam

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // require your server and launch it here
+const server = require("./api/server");
+const port = 3000;
+server.listen(port, () => console.log(`Example app listening on port ${port}`));
