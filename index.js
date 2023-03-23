1ea39a98d4a4cf3373bbf0efba8904761705310d
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-03-23 11:55:31 +0300

    a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // require your server and launch it here
 const server = require("./api/server");
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Example app listening on port ${port}`));
