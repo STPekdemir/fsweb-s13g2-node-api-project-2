bb18c91cb3d17f3dddea3db9033bd02e68d6e2de
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-06-14 19:38:14 +0300

    a

--- api/server.js
+++ api/server.js
@@ -5,5 +5,12 @@
 const express = require("express");
 const server = express();
 const router = require("./posts/posts-router");
+const bodyParser = require("body-parser");
+server.use(bodyParser.json());
 server.use("/api/posts", router);
+server.post("/webhook", (req, res) => {
+  const receivedData = req.body; // Data received from the Typeform webhook
+  console.log("Received webhook data:", receivedData);
+  res.sendStatus(200);
+});
 module.exports = server;
