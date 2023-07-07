a1f6c22c27d706fa32b374a78a84ff445281e588
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-07-07 16:05:36 +0300

    Created 2 new endPoints for charts

--- api/mongoDB/chartData/chartRouter.js
+++ api/mongoDB/chartData/chartRouter.js
@@ -22,5 +22,43 @@ router.get("/pie", async (req, res, next) => {
     next(error);
   }
 });
+router.get("/pieRef", async (req, res, next) => {
+  try {
+    let customers = await Costemers.find().select({ source: 1, _id: 0 });
+    let obj = {};
+    customers.map((item) => {
+      if (obj.hasOwnProperty(item.source)) {
+        obj[item.source] = obj[item.source] + 1;
+      } else {
+        obj[item.source] = +1;
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/countOfPeople", async (req, res, next) => {
+  try {
+    const customers = await Costemers.find().select({ createdAt: 1, _id: 0 });
+    const obj = {};
+    customers.map((item) => {
+      const date = new Date(item.createdAt);
 
+      let fullDate = [
+        date.getDay(),
+        date.getMonth + 1,
+        date.getFullYear(),
+      ].join(".");
+      if (obj.hasOwnProperty(fullDate)) {
+        obj[fullDate] = obj[fullDate] + 1;
+      } else {
+        obj[fullDate] = 1;
+      }
+    });
+    res.status(200).json(obj);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
+let date = new Date("2023-06-23T13:52:31.541+00:00");
