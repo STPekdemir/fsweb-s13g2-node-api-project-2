d52556bdbea4d80865958cf7b37b246fa41f3e8c
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-07-07 13:09:53 +0300

    deneme

--- api/mongoDB/chartData/chartRouter.js
+++ api/mongoDB/chartData/chartRouter.js
@@ -3,17 +3,19 @@ const Costemers = require("../mongoDb-model");
 
 router.get("/pie", async (req, res, next) => {
   try {
-    let customers = await Costemers.find().select("creditScore -_id");
+    let customers = await Costemers.find().select({ creditScore: 1, _id: 0 });
     let obj = {};
-    let data = customers.map((item) => {
+    customers.map((item) => {
       if (obj.hasOwnProperty(item.creditScore)) {
         obj[item.creditScore] = obj[item.creditScore] + 1;
       } else {
         obj[item.creditScore] = 1;
       }
     });
-    res.status(200).json(obj);
+    res.json(obj);
   } catch (error) {
     next(error);
   }
 });
+
+module.exports = router;
