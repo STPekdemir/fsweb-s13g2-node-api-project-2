8a12d5c21974518a08790a1348f9acccbafc568d
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-07-07 14:17:57 +0300

    Fix

--- api/mongoDB/chartData/chartRouter.js
+++ api/mongoDB/chartData/chartRouter.js
@@ -5,16 +5,17 @@ router.get("/pie", async (req, res, next) => {
   try {
     let customers = await Costemers.find().select({ creditScore: 1, _id: 0 });
     let obj = {};
-    if (typeof item.creditScore === "number") {
-      customers.map((item) => {
+
+    customers.map((item) => {
+      if (typeof item.creditScore === "number") {
         let floor = Math.floor(item.creditScore);
         if (obj.hasOwnProperty(floor)) {
           obj[floor] = obj[floor] + 1;
         } else {
           obj[floor] = 1;
         }
-      });
-    }
+      }
+    });
 
     res.json(obj);
   } catch (error) {
